9c308605729cd3a34dd85d51c541359f30e8a464
ruslan <ruslan.rodionow98@gmail.com>
2021-08-02 19:37:10 +0300

    done

--- main.js
+++ main.js
@@ -2,11 +2,9 @@ const electron = require("electron");
 const url = require("url");
 const path = require("path");
 const { create } = require("domain");
+var util = require("util");
 
-//Set env
-process.env.NODE_ENV = "production";
-
-const { app, BrowserWindow, Menu } = electron;
+const { app, BrowserWindow } = electron;
 
 const PORT = 8888;
 let server = null;
@@ -14,7 +12,6 @@ let server = null;
 const PY_DIST_FOLDER = "pydist";
 const PY_MODULE_NAME = "server";
 
-//python server is packaged
 const isPackaged = () => {
 	const fullPath = path.join(__dirname, PY_DIST_FOLDER);
 	return require("fs").existsSync(fullPath);
@@ -28,6 +25,7 @@ const getScriptPath = () => {
 	if (process.platform === "win32") {
 		return path.join(__dirname, PY_DIST_FOLDER, PY_MODULE_NAME, PY_MODULE_NAME + ".exe");
 	}
+
 	return path.join(__dirname, PY_DIST_FOLDER, PY_MODULE_NAME, PY_MODULE_NAME);
 };
 
@@ -35,9 +33,20 @@ const getScriptPath = () => {
 function createServer() {
 	const script = getScriptPath();
 	const cp = require("child_process");
+	// if (!isPackaged()) server = cp.spawn("python", "server.py");
 
-	if (!isPackaged()) server = cp.spawn("python", [script, PORT]);
+	console.log(script);
+
+	if (!isPackaged()) server = cp.spawn("python3", [script, PORT]);
 	else server = cp.execFile(script, [PORT]);
+
+	util.log("readingin");
+
+	process.stdout.on("data", function (chunk) {
+		let textChunk = chunk.toString("utf8"); // buffer to string
+
+		util.log(textChunk);
+	});
 
 	server.stderr.on("error", (err) => {
 		console.log("Server threw an error: " + err);
@@ -53,14 +62,11 @@ app.on("ready", () => {
 	mainWindow = new BrowserWindow({});
 	mainWindow.loadURL(
 		url.format({
-			pathname: path.join(__dirname, "mainWindow.html"),
+			pathname: path.join(__dirname, "index.html"),
 			protocol: "file",
 			slashes: true,
 		}),
 	);
-
-	const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
-	Menu.setApplicationMenu(mainMenu);
 
 	createServer();
 });
@@ -68,38 +74,3 @@ app.on("ready", () => {
 app.on("will-quit", () => {
 	server.kill();
 });
-
-//Define menu template
-const mainMenuTemplate = [
-	{
-		label: "File",
-		submenu: [
-			{
-				label: "Quit",
-				accelerator: process.platform == "darwin" ? "Command+Q" : "Ctrl+Q",
-				click() {
-					app.quit();
-				},
-			},
-		],
-	},
-];
-
-//Add submenus for development
-if (process.env.NODE_ENV !== "production") {
-	mainMenuTemplate.push({
-		label: "Dev Tools",
-		submenu: [
-			{
-				label: "Toggle DevTools",
-				accelerator: process.platform == "darwin" ? "Command+I" : "Ctrl+I",
-				click(item, focusedWindow) {
-					focusedWindow.toggleDevTools();
-				},
-			},
-			{
-				role: "reload",
-			},
-		],
-	});
-}
